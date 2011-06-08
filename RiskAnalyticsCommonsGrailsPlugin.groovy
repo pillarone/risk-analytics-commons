05c1cd5eb1441d6be64920880bcca7dd0895d52f
Jessika Walter <jessika.walter@intuitive-collaboration.com>
2011-06-08 17:24:02 +0200

    new plugin

--- RiskAnalyticsCommonsGrailsPlugin.groovy
+++ RiskAnalyticsCommonsGrailsPlugin.groovy
@@ -8,7 +8,7 @@ import org.pillarone.riskanalytics.core.util.ResourceBundleRegistry
 
 class RiskAnalyticsCommonsGrailsPlugin {
     // the plugin version
-    def version = "0.1.8"
+    def version = "0.1.9"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
     // the other plugins this plugin depends on
