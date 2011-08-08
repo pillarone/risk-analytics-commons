06fcab1b99d179efe96367754888e86a55b5379f
Stefan Kunz <stefan.kunz@intuitive-collaboration.com>
2011-08-08 16:45:18 +0200

    PMO-1795: removing wrong getConstrutionString() method and using default method of super class

--- src/groovy/org/pillarone/riskanalytics/domain/utils/math/copula/AbstractCopulaType.groovy
+++ src/groovy/org/pillarone/riskanalytics/domain/utils/math/copula/AbstractCopulaType.groovy
@@ -15,22 +15,4 @@ abstract class AbstractCopulaType extends AbstractParameterObjectClassifier {
     protected AbstractCopulaType(String displayName, String typeName, Map parameters) {
         super(displayName, typeName, parameters)
     }
-
-    public String getConstructionString(Map parameters) {
-        StringBuffer parameterString = new StringBuffer('[')
-        parameters.each {k, v ->
-            if (v.class.isEnum()) {
-                parameterString << "\"$k\":${v.class.name}.$v,"
-            }
-            else {
-                parameterString << "\"$k\":$v,"
-            }
-        }
-        if (parameterString.size() == 1) {
-            parameterString << ':'
-        }
-        parameterString << ']'
-        "org.pillarone.riskanalytics.domain.pc.generators.copulas.CopulaStrategyFactory.getCopulaStrategy(${this.class.name}.${typeName.toUpperCase()}, ${parameterString})"
-    }
-
 }
