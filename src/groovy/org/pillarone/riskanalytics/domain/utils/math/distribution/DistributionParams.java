04e5c1ad5ec86e30bb33a4405cb2b230d2b8ad54
Simon Parten <simon.parten@art-allianz.com>
2012-06-14 11:56:46 +0200

    Minor formatting, alphabetical order

--- src/groovy/org/pillarone/riskanalytics/domain/utils/math/distribution/DistributionParams.java
+++ src/groovy/org/pillarone/riskanalytics/domain/utils/math/distribution/DistributionParams.java
@@ -4,11 +4,34 @@ package org.pillarone.riskanalytics.domain.utils.math.distribution;
  * @author stefan.kunz (at) intuitive-collaboration (dot) com
  */
 public enum DistributionParams {
-    A("a"), ALPHA("alpha"), B("b"), BETA("beta"), CONSTANT("constant"), CONSTANTS("constants"), CUMULATIVE_PROBABILITES("cumulative probabilities"),
-    DISCRETE_EMPIRICAL_CUMULATIVE_VALUES("discreteEmpiricalCumulativeValues"), DISCRETE_EMPIRICAL_VALUES("discreteEmpiricalValues"),
-    DELTA("delta"), GAMMA("gamma"), LAMBDA("lambda"), MEAN("mean"), MU("mu"), SIGMA("sigma"), STDEV("stDev"), P("p"),
-    OBSERVATIONS("observations"), PERIOD("period"), PROBABILITIES("probabilities"), M("m"), N("n"), CV("CV"),
-    SUPPORT_POINTS("supportPoints"), XI("xi"), TAU("tau"), VALUES("values");
+    A("a"),
+    ALPHA("alpha"),
+    B("b"),
+    BETA("beta"),
+    CONSTANT("constant"),
+    CONSTANTS("constants"),
+    CUMULATIVE_PROBABILITES("cumulative probabilities"),
+    CV("CV"),
+    DISCRETE_EMPIRICAL_CUMULATIVE_VALUES("discreteEmpiricalCumulativeValues"),
+    DISCRETE_EMPIRICAL_VALUES("discreteEmpiricalValues"),
+    DELTA("delta"),
+    GAMMA("gamma"),
+    LAMBDA("lambda"),
+    M("m"),
+    MEAN("mean"),
+    MU("mu"),
+    N("n"),
+    OBSERVATIONS("observations"),
+    P("p"),
+    PERIOD("period"),
+    PROBABILITIES("probabilities"),
+    SIGMA("sigma"),
+    STDEV("stDev"),
+    SUPPORT_POINTS("supportPoints"),
+    TAU("tau"),
+    VALUES("values"),
+    XI("xi")
+    ;
 
     String name;
 
