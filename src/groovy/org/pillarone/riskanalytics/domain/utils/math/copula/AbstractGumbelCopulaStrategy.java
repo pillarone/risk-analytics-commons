db04014b086caf04b5b7a7275bf0b866e4ff78b4
Simon Parten <simon.parten@art-allianz.com>
2014-03-21 11:52:12 +0100

    Newton method diverges if starting point is too far removed from ultimate solution (forgot important condition)

--- src/groovy/org/pillarone/riskanalytics/domain/utils/math/copula/AbstractGumbelCopulaStrategy.java
+++ src/groovy/org/pillarone/riskanalytics/domain/utils/math/copula/AbstractGumbelCopulaStrategy.java
@@ -64,7 +64,7 @@ public abstract class AbstractGumbelCopulaStrategy extends AbstractCopulaStrateg
     double newtonApproximation(double q, double theta, double epsilon, int maxSteps) {
         double xOld = q;
         double xNew;
-        if(q < 0.3d ) {
+        if( theta < 2 || q < 0.3) {
             xNew = q / 10d;
         } else {
             xNew = 1d - q;
